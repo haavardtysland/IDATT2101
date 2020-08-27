d4c1f01ee698ef80ebdd29c8696b126ff9c24747
Håvard Tysland <55196403+haavardtysland@users.noreply.github.com>
2020-08-27 14:04:34 +0200

    Update Rekursjon.java

--- src/Oblig1/Rekursjon.java
+++ src/Oblig1/Rekursjon.java
@@ -8,21 +8,21 @@ public class Rekursjon {
     public static void main(String[] args) {
 
         System.out.println("Test av metodene");
-        System.out.println(oppgave1(2, 10));
-        System.out.println(oppgave2(2, 10));
-        System.out.println(javaMath(2,10));
+        System.out.println("Oppgave 1:" + oppgave1(2, 10));
+        System.out.println("Oppgave 2:" + oppgave2(2, 10));
+        System.out.println("Math.pow :" + javaMath(2,10));
 
         System.out.println("Oppgave 1 tid: ");
-        testTid(1.001, 1000,Rekursjon::oppgave1);
-        testTid(1.001,2000, Rekursjon::oppgave1);
-        testTid(1.001, 4000, Rekursjon::oppgave1);
-        testTid(1.001, 8000,Rekursjon::oppgave1);
+        testTid(1.001, 10,Rekursjon::oppgave1);
+        testTid(1.001,100, Rekursjon::oppgave1);
+        testTid(1.001, 1000, Rekursjon::oppgave1);
+        testTid(1.001, 10000,Rekursjon::oppgave1);
 
         System.out.println("Oppgave 2 tid: ");
-        testTid(1.001, 1000,Rekursjon::oppgave2);
-        testTid(1.001,2000, Rekursjon::oppgave2);
-        testTid(1.001, 4000, Rekursjon::oppgave2);
-        testTid(1.001, 8000,Rekursjon::oppgave2);
+        testTid(1.001, 100,Rekursjon::oppgave2);
+        testTid(1.001,1000, Rekursjon::oppgave2);
+        testTid(1.001, 10000, Rekursjon::oppgave2);
+        testTid(1.001, 100000,Rekursjon::oppgave2);
 
         System.out.println("Math sin pow-metodes tid: ");
         testTid(1.001, 1000,Rekursjon::javaMath);
@@ -35,24 +35,24 @@ public class Rekursjon {
 
 
 
-    public static double oppgave1(double x, int n) {
-        if (n < 0) {
+    private static double oppgave1(double x, int n) {
+        /*if (n < 0 ) {
             throw new IllegalArgumentException("n kan ikke være negativ");
-        }
+        }*/
         if (n == 0) {
             return 1;
         }
         return x * oppgave1(x, n-1);
     }
 
-    public static double oppgave2(double x, int n) {
-        if (n < 0 ) {
+    private static double oppgave2(double x, int n) {
+        /*if (n < 0 ) {
             throw new IllegalArgumentException("n kan ikke være negativ");
-        }
+        }*/
         if(n == 0) {
             return 1;
         }
-        else if(n%2 != 0) {
+        else if(n%2 == 1) {
             return x * oppgave2(x*x, (n-1)/2);
         }
         else if (n%2 == 0) {
@@ -62,11 +62,11 @@ public class Rekursjon {
         }
     }
 
-    public static double javaMath(double x, int n) {
+    private static double javaMath(double x, int n) {
         return Math.pow(x, n);
     }
 
-    public static void testTid(double x, int n, BiFunction<Double, Integer, Double> func) {
+    private static void testTid(double x, int n, BiFunction<Double, Integer, Double> func) {
         Date start = new Date();
         int runder = 0;
         double tid;
