fa8ed742f521be5d9916d08070fc8de5ea5247f5
Håvard Tysland <55196403+haavardtysland@users.noreply.github.com>
2020-08-27 12:03:05 +0200

    Update Rekursjon.java

--- src/Oblig1/Rekursjon.java
+++ src/Oblig1/Rekursjon.java
@@ -1,24 +1,41 @@
 package Oblig1;
 
+import java.sql.SQLOutput;
 import java.util.Date;
+import java.util.function.BiFunction;
 
 public class Rekursjon {
 
     public static void main(String[] args) {
-        Date start = new Date();
-        int runder = 0;
-        double tid;
-        Date slutt;
-        do {
-            oppgave1(1.001, 2000);
-            //System.out.println(oppgave2(1.01, 1000));
-            //System.out.println(Math.pow(1.001, 5000));
-            slutt = new Date();
-            ++runder;
-        } while (slutt.getTime() - start.getTime() < 1000);
-        tid = (double)(slutt.getTime() - start.getTime()) / runder;
-        System.out.println("Tid: " + tid);
+
+        System.out.println("Test av metodene");
+        System.out.println(oppgave1(2, 10));
+        System.out.println(oppgave2(2, 10));
+        System.out.println(javaMath(2,10));
+
+        System.out.println("Oppgave 1 tid: ");
+        testTid(1.001, 100,Rekursjon::oppgave1);
+        testTid(1.001,1000, Rekursjon::oppgave1);
+        testTid( 1.001, 2000, Rekursjon::oppgave1);
+        testTid(1.001, 5000,Rekursjon::oppgave1);
+
+        System.out.println("Oppgave 2 tid: ");
+        testTid(1.001, 100,Rekursjon::oppgave2);
+        testTid(1.001,1000, Rekursjon::oppgave2);
+        testTid( 1.001, 2000, Rekursjon::oppgave2);
+        testTid(1.001, 5000,Rekursjon::oppgave2);
+
+        System.out.println("Math.pow tid: ");
+        testTid(1.001, 100,Rekursjon::javaMath);
+        testTid(1.001,1000, Rekursjon::javaMath);
+        testTid( 1.001, 2000, Rekursjon::javaMath);
+        testTid(1.001, 5000,Rekursjon::javaMath);
+
     }
+
+
+
+
     public static double oppgave1(double x, int n) {
         if (n < 0) {
             throw new IllegalArgumentException("n kan ikke være negativ");
@@ -43,5 +60,23 @@ public class Rekursjon {
         else {
             return oppgave2(x*x, n/2);
         }
+    }
+
+    public static double javaMath(double x, int n) {
+        return Math.pow(x, n);
+    }
+
+    public static void testTid(double x, int n, BiFunction<Double, Integer, Double> func) {
+        Date start = new Date();
+        int runder = 0;
+        double tid;
+        Date slutt;
+        do {
+            func.apply(x, n);
+            slutt = new Date();
+            ++runder;
+        } while (slutt.getTime() - start.getTime() < 1000);
+        tid = (double)(slutt.getTime() - start.getTime()) / runder;
+        System.out.println("Tid: " + tid);
     }
 }
