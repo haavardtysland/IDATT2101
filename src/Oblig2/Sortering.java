6427761db468bd178c497a52978a0cdf501cb371
haavarty <haavarty@stud.ntnu.no>
2020-09-03 09:42:08 +0200

    Update Sortering.java

--- src/Oblig2/Sortering.java
+++ src/Oblig2/Sortering.java
@@ -5,14 +5,18 @@ public class Sortering {
     public static void main(String[] args) {
         int[] random = new int[]{1, 6, 4, 8, 3, 2};
         SinglePivot singlePivot = new SinglePivot();
-        System.out.println("Før sortering: " +sjekkSum(random));
+        System.out.println("Før sortering sum: " +sjekkSum(random));
+        System.out.println("Før sortering rekkefølge: " + rekkefolgeTest(random));
         singlePivot.quicksort(random, random[0], random.length - 1);
-        System.out.println("Etter sortering" + sjekkSum(random));
+        System.out.println("Etter sortering sum: " + sjekkSum(random));
+        System.out.println("Etter sortering rekkefølge: " + rekkefolgeTest(random));
+
         for (int x : random) {
             System.out.println(x);
         }
     }
 
+    //sjekker summen av en array
     public static int sjekkSum(int[] array) {
         int sum = 0;
         for(int i = 0; i < array.length; i++) {
@@ -21,7 +25,13 @@ public class Sortering {
         return sum;
     }
 
-    public static int rekkefolgeTest(int[] array) {
-        
+    //sjekker at rekkefølgen er riktig frem til alle utenom det siste elementet i tabellen
+    public static boolean rekkefolgeTest(int[] array) {
+        for(int i = 0; i < array.length-2; i++) {
+            if(array[i+1] < array[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
