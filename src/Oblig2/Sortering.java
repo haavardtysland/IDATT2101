a24f0c2d141d69e522aad720c4984b0e15771403
haavarty <haavarty@stud.ntnu.no>
2020-09-03 09:35:22 +0200

    Sortering

--- src/Oblig2/Sortering.java
+++ src/Oblig2/Sortering.java
@@ -3,14 +3,25 @@ package Oblig2;
 public class Sortering {
 
     public static void main(String[] args) {
-
+        int[] random = new int[]{1, 6, 4, 8, 3, 2};
+        SinglePivot singlePivot = new SinglePivot();
+        System.out.println("Før sortering: " +sjekkSum(random));
+        singlePivot.quicksort(random, random[0], random.length - 1);
+        System.out.println("Etter sortering" + sjekkSum(random));
+        for (int x : random) {
+            System.out.println(x);
+        }
     }
 
-    public static void singlePivot() {
-
+    public static int sjekkSum(int[] array) {
+        int sum = 0;
+        for(int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
     }
 
-    public static void dualPivot() {
+    public static int rekkefolgeTest(int[] array) {
         
     }
 }
